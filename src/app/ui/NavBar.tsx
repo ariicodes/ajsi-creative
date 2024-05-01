83ca0ec73d6ce73b813bceba8da0dad4a47c0ef1
Ariela Israel <arielaisrael12@gmail.com>
2024-05-01 16:43:06 -0400

    +positioning & Link

--- src/app/ui/NavBar.tsx
+++ src/app/ui/NavBar.tsx
@@ -2,6 +2,7 @@
 
 import { Bars2Icon } from "@heroicons/react/20/solid";
 import Image from "next/image";
+import Link from "next/link";
 import headerLogo from "../../../public/ajsiHeaderLogo.png";
 import MobileNavMenu from "./MobileNavMenu";
 import DesktopNavMenu from "./DesktopNavMenu";
@@ -15,15 +16,15 @@ export default function NavBar() {
   };
 
   return (
-    <nav className="flex h-[70px] w-screen flex-row bg-almond xl:justify-between z-30">
-      <div className="ml-6 mr-12 w-1/2 self-center xl:w-1/12">
+    <nav className="z-30 flex h-[70px] w-screen flex-row bg-almond xl:justify-between">
+      <Link href="/" className="ml-6 mr-12 w-1/2 self-center xl:w-1/12">
         <Image src={headerLogo} alt="" height={26} width={115} />
-      </div>
-      <div className="hidden xl:block self-center">
+      </Link>
+      <div className="z-50 hidden self-center xl:block">
         <DesktopNavMenu />
       </div>
       <div
-        className={`flex w-1/2 flex-col items-end xl:hidden ${!menuOpen ? "self-center" : ""}`}
+        className={`flex w-1/2 flex-col items-end xl:hidden ${!menuOpen ? "self-center" : ""} z-50`}
       >
         <button
           className={`${menuOpen ? "hidden" : ""}`}
