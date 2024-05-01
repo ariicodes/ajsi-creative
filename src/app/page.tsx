3a3fd425fc5eb899f1f10cc94d7ed216d9661df4
Ariela Israel <arielaisrael12@gmail.com>
2024-05-01 16:39:53 -0400

    +SubscriptionForm & MainCTA

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,13 @@
 import HeroSection from "./ui/Home/HeroSection";
+import MainCTA from "./ui/MainCTA";
+import SubscriptionForm from "./ui/SubscriptionForm";
 
 export default function Home() {
   return (
     <main>
       <HeroSection />
+      <SubscriptionForm />
+      <MainCTA />
     </main>
   );
 }
