562d723a92cbdace76e08a026eac66a8cb33f091
Ariela Israel <arielaisrael12@gmail.com>
2024-05-01 16:40:52 -0400

    +SubmitBTn & LightActionBtn; new functionality

--- src/app/ui/Buttons.tsx
+++ src/app/ui/Buttons.tsx
@@ -1,13 +1,40 @@
-interface DarkActionBtnProps {
+"use client";
+import { useRouter } from "next/navigation";
+
+interface BtnProps {
   content: string;
-  onClick: () => void; // Function that takes no arguments and returns nothing (void)
+  route?: string;
 }
 
-export const DarkActionBtn = ({ content, onClick }: DarkActionBtnProps) => {
+export const DarkActionBtn = ({ content, route }: BtnProps) => {
+  const router = useRouter();
   return (
     <button
-      onClick={onClick}
-      className="h-7 w-40 bg-gunmetal pb-0.5 text-center font-black text-base text-white"
+      onClick={() => route && router.push(route)}
+      className="h-7 w-40 bg-gunmetal pb-0.5 text-center font-serif text-base font-black text-white"
+    >
+      {content}
+    </button>
+  );
+};
+
+export const SubmitBtn = ({ content }: { content: string }) => {
+  return (
+    <button
+      type="submit"
+      className="h-7 w-40 bg-cordovan pb-0.5 text-center font-serif text-base font-black text-white"
+    >
+      {content}
+    </button>
+  );
+};
+
+export const LightActionBtn = ({ content, route }: BtnProps) => {
+  const router = useRouter();
+  return (
+    <button
+      onClick={() => route && router.push(route)}
+      className="h-7 w-40 bg-almond pb-0.5 text-center font-serif text-base font-black text-gunmetal"
     >
       {content}
     </button>
