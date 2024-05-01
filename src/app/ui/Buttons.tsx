dcac047e2c28fffdd1f71c35cc6a8b30508d9d87
Ariela Israel <arielaisrael12@gmail.com>
2024-05-01 00:52:53 -0400

    +font edit

--- src/app/ui/Buttons.tsx
+++ src/app/ui/Buttons.tsx
@@ -7,7 +7,7 @@ export const DarkActionBtn = ({ content, onClick }: DarkActionBtnProps) => {
   return (
     <button
       onClick={onClick}
-      className="h-7 w-40 bg-gunmetal pb-0.5 text-center font-black text-white"
+      className="h-7 w-40 bg-gunmetal pb-0.5 text-center font-black text-base text-white"
     >
       {content}
     </button>
