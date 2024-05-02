cda5591bbb1e7dd738f847e97c85803724524819
Ariela Israel <arielaisrael12@gmail.com>
2024-05-02 00:23:30 -0400

    +ServicesList

---
+++ src/app/ui/Services/ServicesList.tsx
@@ -0,0 +1,33 @@
+interface Package {
+  id: number;
+  name: string;
+  services: string[];
+  startingPrice: number;
+}
+
+export default function ServicesList({ services }: { services?: Package[] }) {
+  return (
+    <ul className="min-h-[460px] pb-6 pt-2">
+      {services &&
+        services.map((service) => (
+          <li key={service.id} className="pt-4">
+            <div className="px-6 pb-2">
+              <h2 className="text-xl font-semibold uppercase text-cordovan">
+                {service.name}
+              </h2>
+              <ul>
+                {service.services.map((subService, index) => (
+                  <li key={index} className="-mt-1">
+                    {subService}
+                  </li>
+                ))}
+              </ul>
+            </div>
+            <div className="flex h-[30px] flex-row items-center justify-center bg-cambridge-blue font-serif text-sm text-white">
+              <p>starting @ ${service.startingPrice}</p>
+            </div>
+          </li>
+        ))}
+    </ul>
+  );
+}
