d0f751c09a8784e61e23d0b0786e3541a5c4ae45
Ariela Israel <arielaisrael12@gmail.com>
2024-05-05 18:15:06 -0400

    components renamed to (components)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
-import HeroSection from "./components/home/HeroSection";
-import MainCTA from "./components/MainCTA";
-import SubscriptionForm from "./components/SubscriptionForm";
+import HeroSection from "./(components)/home/HeroSection";
+import MainCTA from "./(components)/MainCTA";
+import SubscriptionForm from "./(components)/SubscriptionForm";
 
 export default function Home() {
   return (
