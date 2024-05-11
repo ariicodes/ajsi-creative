3d414005b045e1c61c87d772af482433f69048c7
Ariela Israel <arielaisrael12@gmail.com>
2024-05-10 22:28:10 -0400

    +loading component

---
+++ src/app/services/loading.tsx
@@ -0,0 +1,9 @@
+export default function Loading() {
+  return (
+    <div className="loading flex h-[629px] flex-col items-center justify-center">
+      <h2 className="text-3xl">
+        Loading...
+      </h2>
+    </div>
+  );
+}
