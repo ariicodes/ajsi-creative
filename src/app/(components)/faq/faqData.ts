c181e29ee322c23771213637cf28db817103cc8a
Ariela Israel <arielaisrael12@gmail.com>
2024-05-10 21:39:03 -0400

    +faqData

---
+++ src/app/(components)/faq/faqData.ts
@@ -0,0 +1,65 @@
+const faqs = [
+  {
+    _id: 1,
+    question: "What services does AJSI Creative provide?",
+    answer:
+      "AJSI Creative offers a variety of services including graphic design, UI/UX design, brand identity design, full-stack software engineering, and photography.",
+  },
+  {
+    _id: 2,
+    question: "Who founded AJSI Creative?",
+    answer:
+      "AJSI Creative was established in 2023 by Ariela Israel, a talented creative professional skilled in graphic design, software engineering, and photography.",
+  },
+  {
+    _id: 3,
+    question: "What makes AJSI Creative unique among other creative agencies?",
+    answer:
+      "AJSI Creative stands out for its transparent and sincere approach, innovative solutions, top-notch design, and robust engineering.",
+  },
+  {
+    _id: 4,
+    question: "How does AJSI Creative approach client projects?",
+    answer:
+      "AJSI Creative takes a collaborative approach, focusing on empowering clients, fostering authenticity, and driving innovation.",
+  },
+  {
+    _id: 5,
+    question: "Does AJSI Creative offer personalized solutions?",
+    answer:
+      "Yes, AJSI Creative specializes in custom-tailored solutions to meet the specific needs of each client, whether it's graphic design, UI/UX design, brand identity, software engineering, or photography.",
+  },
+  {
+    _id: 6,
+    question: "Which industries does AJSI Creative serve?",
+    answer:
+      "While AJSI Creative’s niche is highly focuses on small businesses and entrepreneurs, we cater to a diverse range of industries including technology, startups, e-commerce, hospitality, and more, leveraging its versatile expertise to meet client needs.",
+  },
+  {
+    _id: 7,
+    question: "How does AJSI Creative ensure client satisfaction?",
+    answer:
+      "AJSI Creative prioritizes client satisfaction through open communication, delivering high-quality results, and continually seeking feedback for improvement.",
+  },
+  {
+    _id: 8,
+    question:
+      "What is AJSI Creative's philosophy towards creativity and innovation?",
+    answer:
+      "AJSI Creative believes in merging creativity with functionality, tradition with innovation, and empowering clients to thrive in a dynamic business environment.",
+  },
+  {
+    _id: 9,
+    question: "Can AJSI Creative handle projects of all sizes?",
+    answer:
+      "Yes, AJSI Creative is equipped to handle projects of any scale, from small branding initiatives to large software development projects, thanks to our diverse tool belt of skills and collaborative approach. We, at times may outsource talent if we feel the need to share the workload based on the circumstances of the client and/or project.",
+  },
+  {
+    _id: 10,
+    question: "How can I engage with AJSI Creative?",
+    answer:
+      "To start a project with AJSI Creative, simply reach out via our website’s contact form or contact us directly:",
+  },
+];
+
+export default faqs;
