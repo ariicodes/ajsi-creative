a122d234010056b734ba0631322d9cb70cb4e431
Ariela Israel <arielaisrael12@gmail.com>
2024-05-10 23:27:34 -0400

    +AboutSection

---
+++ src/app/(components)/about/AboutSection.tsx
@@ -0,0 +1,29 @@
+const AboutSection = () => {
+  return (
+    <div className="flex h-[590px] flex-col justify-center p-6 xl:items-center">
+      <p className="text-center text-sm text-gunmetal xl:w-[650px] xl:text-base">
+        AJSI Creative, founded in 2023 and led by Ariela Israel, a solo creative
+        powerhouse with expertise in graphic design, UI/UX design, brand
+        identity design, full-stack software engineering, and photography. AJSI
+        Creative exudes sincerity through transparent interactions, excitement
+        in embracing cutting-edge creativity, competence in delivering top-tier
+        solutions, sophistication in design aesthetics, and ruggedness in robust
+        engineering.
+        <br />
+        <br /> AJSI Creative is dedicated to empowering clients, fostering
+        authenticity, building a community-centric approach, and driving
+        innovation. With a sincere and collaborative mindset, AJSI Creative
+        thrives on continuous improvement, delivering results, and embracing a
+        bold innovation and entrepreneurial spirit.
+        <br />
+        <br /> At AJSI Creative, Ariela merges her diverse skills and values to
+        create a unique space where creativity meets functionality, innovation
+        meets tradition, and clients are not just served but empowered to
+        thrive. Welcome to a world where design, engineering, and photography
+        converge seamlessly to elevate your brand and exceed your expectations.
+      </p>
+    </div>
+  );
+};
+
+export default AboutSection;
