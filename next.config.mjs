ee610814b7d863e15bb4c73c3b2fb5b56401bb74
Ariela Israel <arielaisrael12@gmail.com>
2024-05-14 03:08:56 -0400

    +aws img

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "ajsi-project-assets.s3.amazonaws.com",
+      },
+    ],
+  },
+};
 
 export default nextConfig;
