5f6bbdcbe7a87250f250c53bd2e972b61334d80f
Ariela Israel <arielaisrael12@gmail.com>
2024-05-16 16:50:48 -0400

    +Testimonials complete with mock data

--- src/app/(components)/testimonials/TestimonialsList.tsx
+++ src/app/(components)/testimonials/TestimonialsList.tsx
@@ -10,11 +10,11 @@ const TestimonialsList = ({
   testimonials: Testimonial[];
 }) => {
   return (
-    <div className="flex flex-col gap-5 p-7">
+    <div className="flex flex-col gap-5 p-7 xl:flex xl:min-h-[560px] xl:flex-col xl:items-center xl:justify-around">
       {testimonials &&
         testimonials.map((testimonial, index) => (
-          <div key={index}>
-            <p className="text-[0.9rem] font-semibold uppercase text-cordovan">
+          <div key={index} className="xl:w-[735px]">
+            <p className="text-[0.9rem] font-semibold uppercase text-cordovan xl:text-base">
               &quot;{testimonial.testimonial}&quot;
             </p>
             <p className="text-sm text-gunmetal">
