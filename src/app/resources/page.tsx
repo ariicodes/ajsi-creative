2c041433eda58cac0cfdfb20dbb8a2082521e933
Ariela Israel <arielaisrael12@gmail.com>
2024-05-16 17:01:15 -0400

    +initial page setup

---
+++ src/app/resources/page.tsx
@@ -0,0 +1,11 @@
+import MainCTA from "../(components)/MainCTA";
+import SubscriptionForm from "../(components)/SubscriptionForm";
+
+export default function Resources() {
+  return (
+    <div>
+      <SubscriptionForm />
+      <MainCTA />
+    </div>
+  );
+}
