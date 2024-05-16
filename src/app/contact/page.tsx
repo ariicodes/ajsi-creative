f99a04de0bac8d79c76de95cccd0ec5832519502
Ariela Israel <arielaisrael12@gmail.com>
2024-05-16 17:05:14 -0400

    +basic set up for contact page

---
+++ src/app/contact/page.tsx
@@ -0,0 +1,13 @@
+import Heading from "../(components)/Heading";
+import MainCTA from "../(components)/MainCTA";
+import SubscriptionForm from "../(components)/SubscriptionForm";
+
+export default function Contact() {
+  return (
+    <div>
+      <Heading header="Contact" />
+      <SubscriptionForm />
+      <MainCTA />
+    </div>
+  );
+}
