ecf05c00832f7410034e8599b946c5b8de9335cf
Ariela Israel <arielaisrael12@gmail.com>
2024-05-21 21:19:13 -0400

    +adjusted content

--- src/app/(components)/MainCTA.tsx
+++ src/app/(components)/MainCTA.tsx
@@ -2,28 +2,27 @@ import { LightActionBtn } from "./Buttons";
 
 export default function MainCTA() {
   return (
-    <section className="flex h-[475px] flex-col justify-center bg-gunmetal text-base text-white xl:h-[315px] xl:items-center">
-      <div className="flex flex-col items-center gap-4 p-6 xl:items-start xl:gap-5">
-        <p>
-          We&apos;re passionate about bringing your brand to life through
-          stunning UI/UX designs, robust full-stack development, and captivating
-          branding services.
+    <section className="flex flex-col justify-center bg-gunmetal text-base text-white xl:h-[400px] xl:items-center">
+      <div className="flex flex-col items-center gap-4 px-6 py-10 xl:gap-5 xl:px-80">
+        <p className="text-2xl">
+          Tired of a website that doesn&apos;t convert? Struggling to build a
+          brand identity that resonates? We can help!
         </p>
         <p>
-          Whether you&apos;re looking to enhance your online presence, create a
-          cohesive brand identity, or showcase your products with striking
-          photography, we&apos;ve got you covered.
+          AJSI Creative empowers small businesses and entrepreneurs with the
+          technical and design solutions needed to elevate their brand. Whether
+          you need a website that converts visitors into loyal customers, a
+          captivating brand identity that sets you apart, or professional
+          photography to showcase your products, we offer a comprehensive suite
+          of services designed to bring your vision to life.
         </p>
-        <p>
-          Let&apos;s collaborate to turn your ideas into unforgettable digital
-          experiences.
-        </p>
-        <p className="mb-5 font-serif text-xl font-bold">
-          Book a consultation today and let&apos;s embark on this creative
-          journey together!
+        <p className="mb-5 font-serif text-xl font-medium">
+          Ready to take your brand to the next level? Let&apos;s chat! Book a
+          free consultation today and discover how we can help you create an
+          unforgettable digital experience for your audience.
         </p>
         <div className="self-center">
-          <LightActionBtn content="let's get started" route="/booking" />
+          <LightActionBtn content="book now" route="/booking" />
         </div>
       </div>
     </section>
