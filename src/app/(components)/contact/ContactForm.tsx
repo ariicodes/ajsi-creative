cf85d85e643b31dc4c4a2ac6101e276e89f397d8
Ariela Israel <arielaisrael12@gmail.com>
2024-05-21 22:47:17 -0400

    +ContactForm

---
+++ src/app/(components)/contact/ContactForm.tsx
@@ -0,0 +1,116 @@
+import { SubmitBtn } from "../Buttons";
+
+export default function ContactForm() {
+  return (
+    <form className="flex flex-col gap-3 px-6 py-8 xl:px-80 xl:py-12 font-serif font-bold text-gunmetal">
+      <label className="flex flex-col gap-2">
+        Name
+        <input
+          type="text"
+          name="name"
+          className="h-[30px] w-full border border-gunmetal"
+        />
+      </label>
+      <label className="flex flex-col gap-2">
+        <div>
+          Title <span className="font-normal italic">(if applicable)</span>{" "}
+          <span className="text-[.6rem] font-normal text-cambridge-blue">
+            ex. CEO of Brand Inc; Marketing Director at Brand Corp.
+          </span>
+        </div>
+        <input
+          type="text"
+          name="title"
+          className="h-[30px] w-full border border-gunmetal"
+        />
+      </label>
+      <label className="flex flex-col gap-2">
+        Phone
+        <input
+          type="tel"
+          name="phone"
+          className="h-[30px] w-full border border-gunmetal"
+        />
+      </label>
+      <label className="flex flex-col gap-2">
+        Email
+        <input
+          type="email"
+          name="email"
+          className="h-[30px] w-full border border-gunmetal"
+        />
+      </label>
+      <fieldset className="flex flex-col">
+        <legend>Reason For Contacting</legend>
+
+        <div>
+          <input
+            type="radio"
+            name="reason-for-contacting"
+            id="service-inquiry"
+            value="Service Inquiry"
+            className="size-3 rounded-full border-2 border-gunmetal focus:ring focus:border-brown-sugar"
+          />
+          <label
+            htmlFor="service-inquiry"
+            className="pl-2 font-sans text-xs font-normal"
+          >
+            Service Inquiry
+          </label>
+        </div>
+        <div>
+          <input
+            type="radio"
+            name="reason-for-contacting"
+            id="project-collaboration-subcontracting"
+            value="Project Collaboration/Subcontracting"
+            className="size-3 rounded-full border-2 border-gunmetal focus:ring focus:border-brown-sugar"
+          />
+          <label
+            htmlFor="project-collaboration-subcontracting"
+            className="pl-2 font-sans text-xs font-normal"
+          >
+            Project Collaboration/Subcontracting
+          </label>
+        </div>
+        <div>
+          <input
+            type="radio"
+            name="reason-for-contacting"
+            id="employment-opportunity"
+            value="Employment Opportunity"
+            className="size-3 rounded-full border-2 border-gunmetal focus:ring focus:border-brown-sugar"
+          />
+          <label
+            htmlFor="employment-opportunity"
+            className="pl-2 font-sans text-xs font-normal"
+          >
+            Employment Opportunity
+          </label>
+        </div>
+        <div>
+          <input
+            type="radio"
+            name="reason-for-contacting"
+            id="other"
+            value="other"
+            className="size-3 rounded-full border-2 border-gunmetal focus:ring focus:border-brown-sugar"
+          />
+          <label htmlFor="other" className="pl-2 font-sans text-xs font-normal">
+            Other <span className="italic">(specify below)</span>
+            <div>
+              <input
+                type="text"
+                name="reason-for-contacting"
+                className="h-[30px] w-full border border-gunmetal"
+              />
+            </div>
+          </label>
+        </div>
+      </fieldset>
+      <div className="flex flex-row justify-center pt-6">
+        <SubmitBtn content="submit" />
+      </div>
+    </form>
+  );
+}
--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,11 +1,13 @@
 import Heading from "../(components)/Heading";
 import MainCTA from "../(components)/MainCTA";
 import SubscriptionForm from "../(components)/SubscriptionForm";
+import ContactForm from "../(components)/contact/ContactForm";
 
 export default function Contact() {
   return (
     <div>
       <Heading header="Contact" />
+      <ContactForm />
       <SubscriptionForm />
       <MainCTA />
     </div>
