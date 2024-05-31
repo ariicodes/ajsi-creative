25c0591bb413eeffff8f559c7b15674e413b9b07
Ariela Israel <arielaisrael12@gmail.com>
2024-05-31 01:41:28 -0400

    +ComingSoon component

---
+++ src/app/(components)/ComingSoon.tsx
@@ -0,0 +1,7 @@
+export default function ComingSoon() {
+  return (
+    <div className="loading flex h-[629px] flex-col items-center justify-center">
+      <h2 className="text-3xl">Coming Soon...</h2>
+    </div>
+  );
+}
--- src/app/portfolio/page.tsx
+++ src/app/portfolio/page.tsx
@@ -1,30 +1,32 @@
 import Heading from "../(components)/Heading";
 import SubscriptionForm from "../(components)/SubscriptionForm";
 import MainCTA from "../(components)/MainCTA";
-import ProjectsList from "../(components)/portfolio/ProjectsList";
+// import ProjectsList from "../(components)/portfolio/ProjectsList";
 import dotenv from "dotenv";
+import ComingSoon from "../(components)/ComingSoon";
 dotenv.config();
 
-const url = process.env.NEXT_PUBLIC_API_URL || "http://localhost:5000";
-export default async function Portfolio() {
-  const getProjects = async () => {
-    try {
-      const res = await fetch(`${url}/projects`, {
-        cache: "no-cache",
-      });
-      return res.json();
-    } catch (error: any) {
-      console.log(`Failed to load projects: ${error.message}
-    `);
-    }
-  };
+// const url = process.env.NEXT_PUBLIC_API_URL || "http://localhost:5000";
+export default function Portfolio() {
+  // const getProjects = async () => {
+  //   try {
+  //     const res = await fetch(`${url}/projects`, {
+  //       cache: "no-cache",
+  //     });
+  //     return res.json();
+  //   } catch (error: any) {
+  //     console.log(`Failed to load projects: ${error.message}
+  //   `);
+  //   }
+  // };
 
-  const projects = await getProjects();
+  // const projects = await getProjects();
 
   return (
     <div>
       <Heading header="Portfolio" />
-      <ProjectsList projects={projects} />
+      {/* <ProjectsList projects={projects} /> */}
+      <ComingSoon />
       <SubscriptionForm />
       <MainCTA />
     </div>
